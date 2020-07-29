d26fec602c6333fb94c2cc906dd3c9deac4e5412
Abraao barros lacerda <abraaobarros3@gmail.com>
2020-07-29 13:59:47 -0300

    sum binary solved

--- problems/facebook/binary-sum/index.js
+++ problems/facebook/binary-sum/index.js
@@ -1,1 +1,28 @@
-console.log("asd");
+function sum_binary(bin1, bin2) {
+  let query = 0;
+  let sum = "";
+  for (let i = 0; i <= bin1.length || i <= bin2.length; i++) {
+    [result, query] = sum_digit(
+      bin1[bin1.length - i - 1],
+      bin2[bin2.length - i - 1],
+      query
+    );
+    sum = `${result}${sum}`;
+  }
+  return sum;
+}
+
+function sum_digit(char1, char2, query) {
+  const bin1 = parseBin(char1);
+  const bin2 = parseBin(char2);
+
+  return [(bin1 + bin2 + query) % 2, bin1 + bin2 + query >= 2 ? 1 : 0];
+}
+
+function parseBin(char) {
+  if (!char) return 0;
+  return parseInt(char);
+}
+
+console.log(sum_binary("1111", "1111"));
+console.log(sum_digit("1", "0", 0));
